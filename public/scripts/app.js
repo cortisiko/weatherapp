bb9a72a3ba42d9f45efae4940dc2545980c97ca4
cortisiko <curtis.david7@gmail.com>
2020-01-30 09:13:03 -0500

    returning data to the browser + skycon annimation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,13 @@
 
 const searchElement = document.querySelector("[city-search]");
 const searchBox = new google.maps.places.SearchBox(searchElement)
+const locationElement = document.querySelector('[data-location]')
+const locationStatus = document.querySelector('[data-status]')
+const locationTemperature = document.querySelector('[data-temperature]')
+const locationWindSpeed = document.querySelector('[data-wind]')
+const icon = new Skycons({color:'#221'})
+icon.set('icon','cloudy')
+icon.play()
 
 searchBox.addListener("places_changed",()=>{
 
@@ -22,12 +29,21 @@ fetch('/weather',{
         lattitude: lattitude,
         longitude:longitude
     })
-}).then((response)=>{
-    response.json()
-}).then(data=>{
-    weatherData(data,place.formatted_address)
+}).then((response)=>response.json()).then(data=>{
+   console.log(data)
+   setWeatherData(data,place.formatted_address)
 }).catch(error=>{
     console.log(error)
 })
 
 })
+
+function setWeatherData(data,place){
+    locationElement.textContent = place
+    locationStatus.textContent = data.summary
+    locationTemperature.textContent = data.temperature
+    locationWindSpeed.textContent = data.windSpeed
+
+    icon.set('icon',data.icon)
+    icon.play()
+}
--- server.js
+++ server.js
@@ -3,8 +3,10 @@ if(process.env.NODE_ENV!=='production'){
 require('dotenv').config()
 }
 
- const DARKSKYAPI = process.env.DARKSKYAPI
- 
+ const DARKSKYAPI = process.env.DARK_SKY_API_KEY
+ const openWeatherMapAPI = process.env.Open_Map_API
+
+ const axios = require('axios')
  const express = require('express');
  const app = express()
 
@@ -12,8 +14,21 @@ require('dotenv').config()
  app.use(express.static('public'))
 
  app.post('/weather',(request,response)=>{
-console.log(request.body);
 
+
+  //  console.log(request.body)
+//     console.log(DARKSKYAPI);
+ //    console.log(request.body.lattitude);
+//     console.log(DARKSKYAPI);
+
+const url = `https://api.darksky.net/forecast/${DARKSKYAPI}/${request.body.lattitude},${request.body.longitude}?units=auto`
+const openMap = `http://api.openweathermap.org/data/2.5/weather?lat=${request.body.lattitude}&lon=${request.body.longitude}&APPID=${openWeatherMapAPI}`
+// console.log(url)
+   axios({
+       url:url,
+       responseType: 'json'
+   }).then(data => response.json(data.data))
+   
  })
 
  app.listen(3000,()=>{
